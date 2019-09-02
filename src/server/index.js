29f4ac6ede3b08e5dc9149a9f88aa9c65711d8a0
franzcarl <franz@bisdahin.de>
2019-09-02 11:36:52 +0200

    heroku deploy

---
+++ src/server/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+cont io = require('socket.io')(server);
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('/', (req, res, next) =>
+res.sendFile(__dirname + './index.html'));
+
+io.on('connection', socket => 
+socket.emit ('hello', {message: 'hello from server'}));
+
+server.listen(port);
