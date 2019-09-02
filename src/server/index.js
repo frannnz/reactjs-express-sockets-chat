abeda1bffe032762ef514785040fb777d925ed6e
franzcarl <franz@bisdahin.de>
2019-09-02 11:47:01 +0200

    test3

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-cont io = require('socket.io')(server);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../../build')));
